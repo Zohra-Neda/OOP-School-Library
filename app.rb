622dafbe660705f70549e1154c9da436ead00134
zohra-neda <zohra.neda@gmail.com>
2023-08-30 22:04:07 +0430

    Remove the commented code

--- app.rb
+++ app.rb
@@ -34,7 +34,6 @@ class App
       print 'Has parent permission? [Y/N]: '
       permission = gets[0].capitalize
       permission = permission == 'Y'
-      # @people.push(Student.new(age, name, parent_permission: permission))
       @people << Student.new(nil, age, name, permission)
     when 2
       print 'Specialization: '
