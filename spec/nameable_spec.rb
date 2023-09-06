4ebfd2d88ac280b8a2f5c5b39c070cd1eecef32b
Cosmas Wasswa <cosywas@gmail.com>
2023-09-06 16:46:05 +0300

    Add tests for Nameable class

---
+++ spec/nameable_spec.rb
@@ -0,0 +1,23 @@
+require './nameable'
+
+describe Nameable do
+    it 'raises a NotImplementedError when not implemented in a subclass' do
+      nameable = double('Nameable')
+      allow(nameable).to receive(:correct_name).and_raise(NotImplementedError, 'Subclasses must implement correct_name')
+  
+      expect { nameable.correct_name }.to raise_error(
+        NotImplementedError, 'Subclasses must implement correct_name'
+      )
+    end
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
